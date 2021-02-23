a74a0a10bb3628049282bac3f3f5df2d98b713bd
PauloBA <paulo.bernal.a@gmail.com>
2021-02-23 08:42:31 -0400

    Adjusting init

--- initFiles.py
+++ initFiles.py
@@ -1,7 +1,7 @@
 import os
 
-def initFile(name, fname, fvar = 'args'):
+def initFile(name, fname, fvar = 'args', username = 'me'):
     f = open(f'./kata/{name}.py', 'w')
-    f.write(f'# Solution by PauloBA\n\ndef {fname}({fvar}):\n pass\n\narg = None\n\nprint({fname}(arg))')
+    f.write(f'# Solution by {username}\n\ndef {fname}({fvar}):\n pass\n\narg = None\n\nprint({fname}(arg))')
 
-initFile('maxSubarraySum', 'max_sequence', 'arr')
+initFile('maxSubarraySum', 'max_sequence', 'arr', 'PauloBA')
